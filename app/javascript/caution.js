f3b035f24d20cdf1dc517f541f636e5ac1efb230
yoppedayo8545 <yoppedayo8545@gmail.com>
2021-03-29 00:25:03 +0900

    Bootstrap導入
    JqueryとBootstrap導入による影響のレイアウト修正も行った

---
+++ app/javascript/caution.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function(){
+
+  const pullDown = document.getElementById("nursing_teacher_password")
+  console.log(pullDown)
+  
+  })
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,12 +4,12 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-// require("turbolinks").start()
+require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("../sort")
 require("../btn")
-
+require("../caution")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -17,3 +17,18 @@ require("../btn")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import 'bootstrap'
+import '../src/application.scss'
+
+var moment = require('moment')
+
+var jQuery = require('jquery')
+
+import "daterangepicker"
+
+global.$ = global.jQuery = jQuery;
+window.$ = window.jQuery = jQuery;
+
+global.moment = moment;
+window.moment = moment;
