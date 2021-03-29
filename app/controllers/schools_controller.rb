2992bc2937f576b78ba210595e504e2b67863e21
yoppedayo8545 <yoppedayo8545@gmail.com>
2021-03-29 16:40:42 +0900

    学年自動更新機能追加

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,8 +1,47 @@
 class SchoolsController < ApplicationController
   before_action :authenticate_nursing_teacher!, only: [:index]
+  # before_action :year_update, only: [:index] 
+  before_action :year_check, only: [:index] 
 
   def index
     @nursing_teacher = NursingTeacher.find(current_nursing_teacher.id)
     @students = Student.all.order(:number)
   end
+
+  def class_change
+    @nursing_teacher = NursingTeacher.find(current_nursing_teacher.id)
+    @students = Student.all.order(:number)
+  end
+
+  private
+
+  def year_update
+    @students = Student.all
+    require 'date'
+    now = Date.today 
+    if now.month == 4
+      @students.each do |student|
+      if student.school_year_update == 0
+        begin
+        ActiveRecord::Base.transaction{
+            Student.where.not(school_year_id: 7).update_all("school_year_id = school_year_id + 1") 
+            Student.update_all("school_year_update = school_year_update + 1")
+        }
+        end
+      end
+      end
+    end
+  end
+
+  def year_check
+    @students = Student.all
+    require 'date'
+    now = Date.today
+      @students.each do |student|
+        if now.month != 4 && student.school_year_update == 1
+        student.where.not(school_year_update: 0).update_all("school_year_update = school_year_update - 1")
+        end
+      end
+  end
+
 end
