67720e4383326f8b42774f69cd4f77edea932451
yoppedayo8545 <yoppedayo8545@gmail.com>
2021-06-11 15:14:22 +0900

    カラムを日本語にしたい
    csvのカラムを日本語でも読み込まれるようにしたい。
    うまく読み込まれない…
    
    参考：https://qiita.com/tyamagu2/items/950aad7e67de1592e9d3

--- app/models/student.rb
+++ app/models/student.rb
@@ -26,13 +26,27 @@ class Student < ApplicationRecord
     end
   end
 
+  HEADER_TO_SYM_MAP = {
+    '姓' => :last_name,
+    '名' => :first_name,
+    'セイ' => :last_kana,
+    'メイ' => :first_kana,
+    '学年' => :school_year_id, 
+    'クラス' => :school_class_id,
+    '出席番号' => :number,
+    '性別' => :gender_id,
+    '学校' => :school_id,
+    '先生' => :nursing_teacher_id
+  }
+
   def self.import(file)
     return if file.nil?
     Student.transaction do
+      header_converter = lambda { |h| HEADER_TO_SYM_MAP[h] }
       @num = 0
       @error_nums = []
       @errors = []
-      CSV.foreach(file.path, encoding: 'Shift_JIS:UTF-8', headers: true, skip_blanks: true).with_index(1) do |row, row_number|  
+      CSV.foreach(file.path, encoding: 'Shift_JIS:UTF-8', headers: true, header_converters: header_converter, skip_blanks: true).with_index(1) do |row, row_number|  
         student = find_by(id: row["id"]) || new
         student.attributes = row.to_hash.slice(*updatable_attributes)
         begin
